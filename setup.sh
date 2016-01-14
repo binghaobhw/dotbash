cdb54231f5759ebff4c148af39ea3fd7cf2b1d86
Binghao Wang <scorpio147wbh@gmail.com>
2016-01-14 09:11:33 +0800

    Add tmux dev env

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 CONFIG_DIR="$(dirname $(readlink -f ${BASH_SOURCE[0]}))/config"
 LINK_DIR=$HOME
 for i in $(ls $CONFIG_DIR)
@@ -6,4 +6,10 @@ do
     target_path="${CONFIG_DIR}/$i"
     link_path="${LINK_DIR}/.$i"
     ln -bsv $target_path $link_path
+done
+
+mkdir -p ~/bin
+for i in $(ls bin)
+do
+    ln -bsv "bin/$i" "~/bin/$i"
 done
