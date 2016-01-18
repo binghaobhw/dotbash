98badad9fb8942f4262ff4097dda14c92bc70959
Binghao Wang <scorpio147wbh@gmail.com>
2016-01-18 21:04:24 +0800

    Update setup.sh
    Automatic symbol link creation.

--- setup.sh
+++ setup.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 CONFIG_DIR="$(dirname $(readlink -f ${BASH_SOURCE[0]}))/config"
-LINK_DIR=$HOME
+LINK_DIR="$HOME"
 for i in $(ls $CONFIG_DIR)
 do
     target_path="${CONFIG_DIR}/$i"
     link_path="${LINK_DIR}/.$i"
     ln -bsv $target_path $link_path
 done
+rm -v $LINK_DIR/.bhwang.zsh-theme
 
-mkdir -p ~/bin
-for i in $(ls bin)
+BIN_DIR="$(dirname $(readlink -f ${BASH_SOURCE[0]}))/bin"
+LINK_DIR="$HOME/bin"
+mkdir -p $LINK_DIR
+for i in $(ls $BIN_DIR)
 do
-    ln -bsv "bin/$i" "~/bin/$i"
+    ln -bsv "$BIN_DIR/$i" "$LINK_DIR/$i"
 done
